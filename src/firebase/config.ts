b9f2e161c0efb03f261f010f0f221630d656e769
Sunil Pinto <swasthllc@gmail.com>
2025-10-06 19:36:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6718588364-20b08",
+  "appId": "1:964792797835:web:2b54fe15fe56122448628e",
+  "apiKey": "AIzaSyCJJL0EWcaxcTikABX5zAB_uqqD4WYuauE",
+  "authDomain": "studio-6718588364-20b08.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "964792797835"
+};
