d84ce5d86eb77ff5c53e45129449bb3de3e5ed6a
Sunil Pinto <swasthllc@gmail.com>
2025-10-15 19:29:14 +0000

    Build Error
    Module not found: Can't resolve '@genkit-ai/firebase/plugin

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {firebase} from '@genkit-ai/firebase/plugin';
+import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [firebase(), googleAI()],
