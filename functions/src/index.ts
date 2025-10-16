836b0b9569adc952173f80949bebfce9316c60ea
Sunil Pinto <swasthllc@gmail.com>
2025-10-16 21:18:27 +0000

    firebase deploy --only functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,12 +8,20 @@
  */
 
 import {setGlobalOptions} from "firebase-functions/v2";
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
 // Import the function from its submodule
 import { sendAppointmentEmail } from "./email-notifications";
 
 // Set global options for the functions
 setGlobalOptions({ maxInstances: 10 });
+
+// This is a simple function to force a redeployment.
+export const forceRedeploy = onRequest((request, response) => {
+    logger.info("Forcing function redeployment.", {structuredData: true});
+    response.send("Deployment forced.");
+});
 
 // Export the function so that Firebase can discover and deploy it
 export { sendAppointmentEmail };
