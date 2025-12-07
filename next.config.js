79ab19fe96f3963f305ac119e4cf0d5117b3e6d9
Sunil Pinto <swasthllc@gmail.com>
2025-12-07 03:06:52 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -39,4 +36,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
