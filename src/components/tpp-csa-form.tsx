08d9c32b41bf2ba148a8fb58f29b465d062dc7dd
Sunil Pinto <swasthllc@gmail.com>
2026-01-05 18:58:22 +0000

    3. COMPLAINTS: To file a complaint, you may contact the manager listed a

--- src/components/tpp-csa-form.tsx
+++ src/components/tpp-csa-form.tsx
@@ -734,12 +734,13 @@ provisions of state and federal law. A separate FirstLight Home Care Private Pay
                     <h3 className="text-lg font-semibold text-center underline">TERMS AND CONDITIONS</h3>
                     <ol className="space-y-4 text-sm text-muted-foreground list-decimal list-inside">
                         <li>
-                            <span className="font-bold">BUSINESS OPERATIONS:</span> FirstLight Home Care is independently owned and operated as a franchisee of FirstLight Home Care
-                            Franchising, LLC. FirstLight Home Care meets all requirements of the State of <strong>California</strong> to provide non-medical in-home personal care, companion and homemaker services. Additional information about FirstLight Home Care that is required to be disclosed under the state law can be found in Section 15 of this Agreement.
+                            <span className="font-bold">BUSINESS OPERATIONS:</span> FirstLight Home Care is independently owned and operated as a franchisee of FirstLight Home Care Franchising, LLC. FirstLight Home Care meets all requirements of the State of <strong>California</strong> to provide non-medical in-home personal care, companion and homemaker services. Additional information about FirstLight Home Care that is required to be disclosed under the state law can be found in Section 15 of this Agreement.
                         </li>
                         <li>
-                            <span className="font-bold">FIRSTLIGHT CONTACT INFORMATION:</span> If you have any question, problems, needs or concerns, please contact the FirstLight
-                            Home Care of Rancho Cucamonga contact Lolita Pinto at 9093214466 or by mail sent to the address above.
+                            <span className="font-bold">FIRSTLIGHT CONTACT INFORMATION:</span> If you have any question, problems, needs or concerns, please contact the FirstLight Home Care of Rancho Cucamonga contact Lolita Pinto at 9093214466 or by mail sent to the address above.
+                        </li>
+                        <li>
+                            <span className="font-bold">COMPLAINTS:</span> To file a complaint, you may contact the manager listed above or the appropriate State reporting agency. In cases of allegations of abuse or neglect by an employee of FirstLight Home Care a complete investigation will be completed as soon as possible, and FirstLight Home Care will complete a written report within 14 days of the initial complaint unless state law requires earlier reporting in which case that requirements shall apply. The written report shall include the date, time, and description of alleged abuse, neglect, or financial exploitation; description of any injury or abuse of the Client; any actions taken by FirstLight Home Care; a description of actions taken to prevent future abuse or other crime, or when death (other than by disease or actual causes) has occurred.
                         </li>
                     </ol>
                 </div>
