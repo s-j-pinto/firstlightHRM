dbb0b25785d484aee161ab915618ec4311aa778b
Sunil Pinto <swasthllc@gmail.com>
2026-02-12 22:17:49 +0000

    seperator PERSONAL seperator

--- src/app/candidate-hiring-forms/hcs501/page.tsx
+++ src/app/candidate-hiring-forms/hcs501/page.tsx
@@ -45,6 +45,17 @@ export default function HCS501Page() {
                         </div>
                     </div>
                 </div>
+
+                <div className="relative my-8">
+                  <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                  </div>
+                  <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-card px-2 text-muted-foreground">
+                      Personal
+                    </span>
+                  </div>
+                </div>
             </CardContent>
         </Card>
     );
